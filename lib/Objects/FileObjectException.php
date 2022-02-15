bb75e4b24ea46ab4742fbe60ca3074e2c4dcd3bb
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-02-15 12:18:43 +0100

    Create FileObjectException.php

---
+++ lib/Objects/FileObjectException.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @file FileObjectException.php
+ * Provides the FileObjectException as a exception class for file objcts. 
+ * Lang en
+ * Reviewstatus: 2021-04-19
+ * Localization: no localization
+ * Documentation: complete
+ * Tests: none
+ * Coverage: none
+ * Dependencies: Sunhill\Files\FilesException
+ */
+
+namespace Sunhill\Files\Objects;
+
+use Sunhill\Files\FilesException;
+
+/**
+ * Basic exception for exceptions that are raised by fileobjects
+ *
+ * @author lokal
+ *        
+ */
+class FileObjectException extends FilesException {};
