d1c1937ac1418609c707a5fac98a03fcb94c3909
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-02-18 23:05:53 +0100

    + Fileobjects

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Sunhill\Crawler\Managers\FileManager;
 use Sunhill\Crawler\Managers\Utils;
+use Sunhill\Crawler\Managers\FileObjects;
 use Sunhill\Basic\Facades\Checks;
 use Sunhill\Crawler\Checks\CheckFileDatabase;
 
@@ -21,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
         $this->app->alias(FileManager::class,'filemanager');
         $this->app->singleton(Utils::class, function () { return new Utils(); } );
         $this->app->alias(Utils::class,'utils');
+        $this->app->singleton(FileObjects::class, function () { return new FileObjects(); } );
+        $this->app->alias(FileObjects::class,'fileobjects');
         Checks::installChecker(CheckFileDatabase::class);
     }
 
