8731ee2fd6db42883c5e948f6de62c785609d23a
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-02-28 13:05:31 +0100

    Create Friend.php

---
+++ lib/Objects/Friend.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * @file Friend.php
+ * Provides informations about a friend (a person that we want to know more informations about)
+ * Lang en
+ * Reviewstatus: 2022-02-28
+ * Localization: unknown
+ * Documentation: unknown
+ * Tests: unknown
+ * Coverage: unknown
+ * Dependencies: Person
+ */
+namespace Sunhill\Crawler\Objects;
+
+/**
+ * The class for friends
+ *
+ * @author lokal
+ *        
+ */
+class Friend extends Person
+{
+    public static $table_name = 'friends';
+    
+    public static $object_infos = [
+        'name'=>'Friend',       // A repetition of static:$object_name @todo see above
+        'table'=>'friends',     // A repitition of static:$table_name
+        'name_s' => 'friend',
+        'name_p' => 'friends',
+        'description' => 'Class for friends',
+        'options'=>0,           // Reserved for later purposes
+    ];
+    
+    protected static function setup_properties()
+    {
+        parent::setup_properties();
+        self::date('date_of_birth')
+            ->set_description('The birthday of this person')
+            ->set_default(null)
+            ->set_displayable(true)
+            ->set_editable(true)
+            ->set_groupeditable(false)
+            ->searchable();
+        self::object('address')
+            ->set_allowedObjects(['Address'])
+            ->set_default(null)
+            ->set_description('The address of this person')
+            ->set_displayable(true)
+            ->set_editable(true)
+            ->set_groupeditable(false)
+            ->searchable();
+        self::arrayOfStrings('friendgroups')
+            ->set_description('What friend groups is this person member of');
+    }
+    
+}
