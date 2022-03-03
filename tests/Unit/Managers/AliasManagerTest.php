65d724c61c40fbc091f9ace85f81bd236e734ece
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-03-03 15:27:09 +0100

    Create AliasManagerTest.php

---
+++ tests/Unit/Managers/AliasManagerTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace Sunhill\Crawler\Tests\Unit;
+
+use Sunhill\Crawler\Facades\AliasManager;
+use Tests\CreatesApplication;
+
+class FileManagerTest extends SunhillScenarioTestCase
+{
+    
+    use CreatesApplication;
+
+}
