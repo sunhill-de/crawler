c683a42c44b9b63c7f5f599da645cd121ed6f908
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-03-03 15:25:43 +0100

    Create AliasManager.php

---
+++ lib/Managers/AliasManager.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @file AliasManager.php
+ * Provides the AliasManager object for handling of Object aliases
+ * Lang en
+ * Reviewstatus: 2022-03-03
+ * Localization: unknown
+ * Documentation: unknown
+ * Tests: tests/Unit/Manager/AliasManagerTest.php
+ * Coverage: unknown
+ */
+namespace Sunhill\Crawler\Managers;
+
+class AliasManager
+{
+}  
