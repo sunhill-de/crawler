6bc9968d2e6670bc94e6a8f8df10bc9a98d97f74
Klaus Dimde <klaus@dimde.info>
2022-03-07 20:28:52 +0100

    Added some documentation and some debug messages

--- lib/Handler/HandlerFileStatus.php
+++ lib/Handler/HandlerFileStatus.php
@@ -10,8 +10,17 @@ use Sunhill\Basic\Utils\Descriptor;
 /**
  * Checks if the file exists and if its readable respective writeable
  * @author klaus
- * Depends: nother
- * Modifies: filestate
+ * Depends: none
+ * Modifies: 
+ * - filestate.exists
+ * - filestate.type
+ * - filestate.current_location
+ * - filestate.readable
+ * - filestate.writeable
+ * - filestate.sha1_hash
+ * - filestate.inMedia
+ * - filestate.alreadyInMedia
+ * - source
  * Condition: none
  */
 class HandlerFileStatus extends HandlerBase
@@ -25,14 +34,18 @@ class HandlerFileStatus extends HandlerBase
             
             if (is_link($descriptor->getCurrentLocation())) {
                 $descriptor->filestate->type = 'link';
+                $this->verboseinfo("  Detecting a link");
                 
                 if ($target = FileManager::normalizeFile(readlink($descriptor->getCurrentLocation()))) {
                     $descriptor->setCurrentLocation($target);                    
+                    $this->verboseinfo("  Link points to $target.");
                 }                
             } else if (is_file($descriptor->getCurrentLocation())) {
                 $descriptor->filestate->type = 'file';
+                $this->verboseinfo("  Detecting a file");
             } else {
                 $descriptor->filestate->type = 'unknown';
+                $this->verboseinfo("  Unknown file type");
             }
 
             $descriptor->filestate->readable = is_readable($descriptor->getCurrentLocation());
@@ -40,11 +53,20 @@ class HandlerFileStatus extends HandlerBase
             
             if ($descriptor->filestate->readable = is_readable($descriptor->getCurrentLocation())) {
                 $descriptor->filestate->sha1_hash = sha1_file($descriptor->getCurrentLocation());                
+                $this->verboseinfo("  Hash is '".$descriptor->filestate->sha1_hash."'");
             }
 
-            $descriptor->filestate->inMedia = FileManager::fileInDir($descriptor->getCurrentLocation(),FileManager::getMediaDir());
+            if ($descriptor->filestate->inMedia = FileManager::fileInDir($descriptor->getCurrentLocation(),FileManager::getMediaDir())) {
+                $descriptor->filestate->alreadyInMedia = true;    
+                $this->verboseinfo("  Scanning inside media");
+                
+            } else {
+                $descriptor->filestate->alreadyInMedia = false;
+                $this->verboseinfo("  Scanning outside media");
+            }
             
         } else {
+            $this->verboseinfo("  File does not exist.");
             $descriptor->filestate->readable = false;
             $descriptor->filestate->writeable = false;
             $descriptor->filestate->type = 'unknown';
