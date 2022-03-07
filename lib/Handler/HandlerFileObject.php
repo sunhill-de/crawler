ecbdec09159bcef158a45e2c99dad04c3272bd56
Klaus Dimde <klaus@dimde.info>
2022-03-07 20:48:28 +0100

    Merged HandlerHash functionallity

--- lib/Handler/HandlerFileObject.php
+++ lib/Handler/HandlerFileObject.php
@@ -8,9 +8,18 @@ use Sunhill\Basic\Utils\Descriptor;
 use Sunhill\Crawler\Objects\File;
 
 /**
- * Calculates the hash of the file and checks if this hash is already in the Database
+ * Checks if this file is already in the database. If yes, load it and fill some fields
+ * If not, create the best fitting file object 
  * @author klaus
- *
+ * Depends: none
+ * Modifies: 
+ * - filestate.mime
+ * - dbstate.isInDatabase
+ * - dbstate.wasInDatabase
+ * - dbstate.id
+ * - file
+ * - source
+ * Condition: none
  */
 class HandlerFileObject extends HandlerBase
 {
@@ -19,8 +28,41 @@ class HandlerFileObject extends HandlerBase
     
     function process(CrawlerDescriptor $descriptor)
     {
+        if ($file = $this->searchHash($descriptor->fileinfo->hash,$descriptor)) {
+            $this->alreadyInDatabase($descriptor,$file);
+        } else {
+            $this->notInDatabase($descriptor);
+        }        
     }
 
+    protected function searchHash($hash,$descriptor)
+    {
+        if ($result = File::search()->where('sha1_hash','=',$hash)->loadIfExists()) {
+            $this->verboseinfo(" Hash already in database");
+            return $result;
+        } else {
+            $this->verboseinfo(" Hash not in database");
+            return false;
+        }
+    }
+    
+    protected function alreadyInDatabase(CrawlerDescriptor $descriptor, File $file)
+    {
+        $descriptor->file = $file;
+        $descriptor->dbstate->isInDatabase = true;
+        $descriptor->dbstate->wasInDatabase = true;
+        $descriptor->dbstate->id = $file->getID();
+        $descriptor->filestate->mime = $file->mime->mime;
+    }
+    
+    protected function notInDatabase(CrawlerDescriptor $descriptor, File $file)
+    {
+        $descriptor->dbstate->isInDatabase = false;
+        $descriptor->dbstate->wasInDatabase = false;
+        $descriptor->dbstate->id = false;
+        $descriptor->filestate->mime = $this->
+    }
+        
     function matches(CrawlerDescriptor $descriptor): Bool
     {
         return $descriptor->fileReadable();
---
+++ tests/Unit/Handler/HandlerFileObjectTest.php
@@ -0,0 +1,78 @@
+<?php
+
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
+use Sunhill\Basic\Tests\SunhillScenarioTestCase;
+use Sunhill\Crawler\CrawlerDescriptor;
+use Sunhill\Crawler\Handler\HandlerFileStatus;
+use Tests\CrawlerTestCase;
+use Tests\CreatesApplication;
+use Tests\Scenarios\ComplexScanScenario;
+
+class HandlerFileObjectTest extends SunhillScenarioTestCase
+{
+ 
+    use CreatesApplication;
+    
+    protected function GetScenarioClass()
+    {
+        return ComplexScanScenario::class;
+    }
+    
+    /**
+     * @dataProvider FileStatusProvider
+     * @param unknown $file
+     * @param unknown $field
+     * @param unknown $expect
+     * @param number $mode
+     */
+    public function testFileStatus($file,$group,$field,$expect,$mode=0777)
+    {
+        $source = $this->getTempDir().$file;
+        chmod($source,$mode);
+        
+        $descriptor = new CrawlerDescriptor();
+        $descriptor->setSource($source);
+        
+        Config::set("crawler.media_dir",$this->getTempDir().'media');
+        $test = new HandlerFileStatus(null);
+        $test->process($descriptor);
+        
+        $this->assertEquals($expect,$descriptor->$group->$field);
+        
+    }
+    
+    public function FileStatusProvider()
+    {
+        return [
+            ['/scan/A.txt','exists',true],  
+            ['/scan/A.txt','readable',true],
+            ['/scan/A.txt','writeable',true],
+            ['/scan/A.txt','type','file'],
+            ['/scan/A.txt','writeable',false,0444],
+            ['/scan/A.txt','inMedia',false],
+            ['/media/originals/6/d/c/6dcd4ce23d88e2ee9568ba546c007c63d9131c1b.txt','inMedia',true],
+            ['/media/source/all/some/dir/link.txt','type','link'],
+            ['/media/source/all/some/dir/link.txt','sha1_hash','6dcd4ce23d88e2ee9568ba546c007c63d9131c1b'],
+            ['/media/source/all/some/dir/link.txt','inMedia',true],
+        ];
+    }
+    
+    public function testUnreadableFile()
+    {
+        $temp = $this->getTempDir();
+        
+        $descriptor = new CrawlerDescriptor();
+        $descriptor->source = "/etc/shadow"; // Better not run as root
+        
+        $test = new HandlerFileStatus(null);
+        $test->process($descriptor);
+        
+        $this->assertTrue($descriptor->filestate->exists);
+        $this->assertFalse($descriptor->filestate->readable);
+        $this->assertFalse($descriptor->filestate->writeable);
+        $this->assertEquals('file',$descriptor->filestate->type);
+    }
+    
+}
