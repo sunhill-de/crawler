44e686b54b59d0a4794447afd8dabe9656ad7950
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-03-15 13:50:59 +0100

    Update list_objects.blade.php

--- resources/views/objects/list_objects.blade.php
+++ resources/views/objects/list_objects.blade.php
@@ -1,33 +1,16 @@
-@extends('templates.list')
+@extends('templates.objectlist')
 
 @section('title','Alle Objekte auflisten')
 
-@section('table')
-       <table>
-        <th>
-         <td>ID</td>
+@section('list_header')
          <td>UUID</td>
          <td>Klasse</td>
          <td>Erzeugt</td>
-         <td>&Ge&auml;ndert</td>
-         <td>&nbsp;</td>
-         <td>&nbsp;</td>
-        </th>
-        @forelse($objects as $object)
-        <tr>
-         <td>{{$object->id}}</td>
+@endsection
+
+@section('list_row')
          <td>{{$object->uuid}}</td>
          <td><a href="/objects/list/{{$object->class}}">{{$object->class}}</a></td>
          <td>{{$object->created}}</td>
          <td>{{$object->changed}}</td>
-         <td><a href="/objects/show/{{$object->id}}">anzeigen</a></td>
-         <td><a href="/objects/edit/{{$object->id}}">bearbeiten</a></td>
-         <td><a href="/objects/delete/{{$object->id}}">l&ouml;schen</a></td>
-        </tr>
-        @empty
-        <tr>
-         <td colspan="5">Keine Eintr&auml;ge</td>
-        </tr> 
-        @endforelse
-       </table>
 @endsection
