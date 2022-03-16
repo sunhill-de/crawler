5f7602400ac1582e7086e491f3a4ba0ef5700c6f
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-03-16 12:24:34 +0100

    + crawler.css

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/css/crawler.css', 'App\Http\Controllers\SystemController@css');
 
 Route::get('/', function () {
     return view('index');
