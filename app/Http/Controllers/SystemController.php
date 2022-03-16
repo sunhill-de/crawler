9f0cd979f42aa175b0d276ecb5bfb768f4adfba3
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-03-16 12:29:31 +0100

    Create SystemController.php

---
+++ app/Http/Controllers/SystemController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App/Http/Controllers;
+
+class SystemController extends Controller
+{
+      public function css() {
+        $content = view('layouts.build',[
+            'files'=>$this->get_files('css')
+        ]);
+        return response($content)->header('Content-Type','text/css');        
+    }
+    
+    public function js() {
+        $content = view('layouts.build',[
+            'files'=>$this->get_files('js')
+        ]);
+        return response($content)->header('Content-Type','text/javascript');
+    }
+    
+    protected function get_files(string $dir) {
+        $result = [];
+        $basedir = base_path('/resources/'.$dir);
+        $files = [];
+        $d = dir($basedir);
+        while (false !== ($entry = $d->read())) {
+            if (is_file($basedir.'/'.$entry)) {
+                $files[] = $basedir.'/'.$entry;
+            }
+        }
+        $d->close(); 
+        sort($files);
+        foreach ($files as $file) {
+            $result[] = file_get_contents($file);
+        }
+        return $result;
+    }
+}
