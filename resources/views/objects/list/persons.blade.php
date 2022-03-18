3005565d0ae014f49a7297861b3786487bfb6c09
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-03-18 11:35:19 +0100

    Update persons.blade.php

--- resources/views/objects/list/persons.blade.php
+++ resources/views/objects/list/persons.blade.php
@@ -2,14 +2,36 @@
 
 @section('title','Alle Personen auflisten')
 
-@section('list_header')
-         <td>Vorname</td>
-         <td>Nachname</td>
-         <td>Geschlecht</td>
-@endsection
-
-@section('list_row')
-         <td>{{$object->firstname}}</td>
-         <td>{{$object->lastname}}</td>
-         <td>{{$object->sex}}</td>
+@section('table')
+       <table>
+        <tr>
+         <th>ID</th>
+         <th>Titel</th>
+         <th>Vorname</th>
+         <th>Nachname</th>
+         <th>Geschlecht</th>
+         <th>&nbsp;</th>
+         <th>&nbsp;</th>
+         <th>&nbsp;</th>
+        </tr>
+        @forelse($objects as $object)
+        <tr>
+         <td>{{$object->id}}</td>
+         <td><a href="/objects/list/{{strtolower($object::$object_infos['name'])}}">{{$object::$object_infos['name']}}</a></td>
+         <td>{{$object->title}}</td> 
+         <td>{{$object->firstname}}</td> 
+         <td>{{$object->lastname}}</td> 
+         <td>{{$object->sex}}</td> 
+         <td><a href="/objects/show/{{$object->id}}">anzeigen</a></td>
+         <td><a href="/objects/edit/{{$object->id}}">bearbeiten</a></td>
+         <td><a href="/objects/delete/{{$object->id}}">l&ouml;schen</a></td>
+        </tr>
+        @empty
+        <tr>
+         <td colspan="8">Keine Eintr&auml;ge</td>
+        </tr> 
+        @endforelse
+       </table>
+       <a href="/">&Uuml;bersicht</a>
+       
 @endsection
