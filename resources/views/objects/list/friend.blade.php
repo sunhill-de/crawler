b397534e20180cf74233e45546296690b15596ef
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-03-18 14:34:13 +0100

    Update friend.blade.php

--- resources/views/objects/list/friend.blade.php
+++ resources/views/objects/list/friend.blade.php
@@ -1,13 +1,39 @@
-@extends('templates.list_persons')
+@extends('templates.objectlist')
 
-@section('title','Alle Personen auflisten')
+@section('title','Alle Freunde auflisten')
 
-@section('list_header')
-         @parent
-         <td>Geburtstag</td>
-@endsection
-
-@section('list_row')
-         @parent
-         <td>{{$object->date_of_birth}}</td>
+@section('table')
+       <table>
+        <tr>
+         <th>ID</th>
+         <th>Titel</th>
+         <th>Vorname</th>
+         <th>Nachname</th>
+         <th>Geschlecht</th>
+         <th>Geburtsdatum</th>
+         <th>&nbsp;</th>
+         <th>&nbsp;</th>
+         <th>&nbsp;</th>
+        </tr>
+        @forelse($objects as $object)
+        <tr>
+         <td>{{$object->id}}</td>
+         <td><a href="/objects/list/{{strtolower($object::$object_infos['name'])}}">{{$object::$object_infos['name']}}</a></td>
+         <td>{{$object->title}}</td> 
+         <td>{{$object->firstname}}</td> 
+         <td>{{$object->lastname}}</td> 
+         <td>{{$object->sex}}</td> 
+         <td>{{$object->date_of_birth}}</td> 
+         <td><a href="/objects/show/{{$object->id}}">anzeigen</a></td>
+         <td><a href="/objects/edit/{{$object->id}}">bearbeiten</a></td>
+         <td><a href="/objects/delete/{{$object->id}}">l&ouml;schen</a></td>
+        </tr>
+        @empty
+        <tr>
+         <td colspan="8">Keine Eintr&auml;ge</td>
+        </tr> 
+        @endforelse
+       </table>
+       <a href="/">&Uuml;bersicht</a>
+       
 @endsection
