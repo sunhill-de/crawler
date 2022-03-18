c8466912f109ae59a755b49189353cfd1a9ba345
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-03-18 13:05:10 +0100

    Create object.blade.php

---
+++ resources/views/objects/show/object.blade.php
@@ -0,0 +1,16 @@
+@extends('templates.objectlist')
+
+@section('title','Objekt anzeigen')
+
+@section('show')
+    <a href="/objects/edit/{{$object->id}}">bearbeiten</a>
+    <a href="/objects/delete/{{$object->id}}">l&ouml;schen</a>
+    <h1>Klasseninfo</h1>
+    <div class="label">Klasse</div>:<div class="value"><a href="/objects/list/{{strtolower($object::$object_infos['name'])}}">{{$object::$object_infos['name']}}</a></div>    
+    <div class="label">Tabellenname</div>:<div class="value">{{$object::$object_infos['tablename']}}</div>    
+    <h1>ORMObject</h1>
+    <div class="label">ID</div>:<div class="value">{{$object->id}}</div>
+    <div class="label">UUID</div>:<div class="value">{{$object->uuid}}</div>
+    <div class="label">Erzeugt</div>:<div class="value">{{$object->created_at}}</div>
+    <div class="label">Verändert</div>:<div class="value">{{$object->updated_at}}</div>
+@endsection
