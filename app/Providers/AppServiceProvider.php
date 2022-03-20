871f8c333b91dee415d59a3ddc7f8b9c2d1b8676
Klaus Dimde <klaus@dimde.info>
2022-03-20 04:21:39 +0100

    Added some classes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,7 @@ use Sunhill\Crawler\Objects\Friend;
 use Sunhill\Crawler\Objects\Genre;
 use Sunhill\Crawler\Objects\Link;
 use Sunhill\Crawler\Objects\MediaDevice;
+use Sunhill\Crawler\Objects\Medium;
 use Sunhill\Crawler\Objects\Mime;
 use Sunhill\Crawler\Objects\MobileDevice;
 use Sunhill\Crawler\Objects\NetworkDevice;
@@ -57,21 +58,22 @@ class AppServiceProvider extends ServiceProvider
        $this->app->alias(FileObjects::class,'fileobjects');
        Checks::installChecker(CheckFileDatabase::class);
        
-       Classes::registerClass(FileObject::class);
-       Classes::registerClass(Dir::class);
-       Classes::registerClass(File::class);
-       Classes::registerClass(Link::class);
-       Classes::registerClass(Mime::class);
-       Classes::registerClass(Person::class);
-       Classes::registerClass(Friend::class);
-       Classes::registerClass(FamilyMember::class);
-       Classes::registerClass(Property::class);
-       Classes::registerClass(ElectronicDevice::class);
-       Classes::registerClass(NetworkDevice::class);
-       Classes::registerClass(MobileDevice::class);
        Classes::registerClass(Computer::class);
-       Classes::registerClass(MediaDevice::class);
-       Classes::registerClass(Network::class);
+       Classes::registerClass(Dir::class);
+       Classes::registerClass(ElectronicDevice::class);
+       Classes::registerClass(FamilyMember::class);
+       Classes::registerClass(File::class);
+       Classes::registerClass(FileObject::class);
+       Classes::registerClass(Friend::class);
        Classes::registerClass(Genre::class);
+       Classes::registerClass(Link::class);
+       Classes::registerClass(MediaDevice::class);
+       Classes::registerClass(Medium::class);
+       Classes::registerClass(Mime::class);
+       Classes::registerClass(MobileDevice::class);
+       Classes::registerClass(Network::class);
+       Classes::registerClass(NetworkDevice::class);
+       Classes::registerClass(Person::class);
+       Classes::registerClass(Property::class);
     }
 }
