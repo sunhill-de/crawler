59681fa3a52d430a54b631a69a9bbbafcdb33ecb
Klaus Dimde <klaus@dimde.info>
2022-03-20 04:17:52 +0100

    Controller for ajax requests

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Sunhill\Crawler\Objects\Person;
+use Sunhill\ORM\Facades\Classes;
+
+class ApiController extends Controller
+{
+
+    public function objectSearch(Request $request,Response $response) 
+    {
+        $allowedClasses = $request->input('allowedObjects');
+        $result = [];
+        
+        foreach ($allowedClasses as $class) {
+            $result = array_merge($result,$this->findObjects($class,$request->input('phrase')));
+        }
+        $search = Person::search()->where('lastname','begins with',$request->input('phrase'))->get();
+        foreach ($search as $person) 
+        {
+            $obj = new \StdClass();
+            $obj->name = $person->firstname." ".$person->lastname;
+            $obj->id = $person->id;
+            $result[] = $obj;
+        }
+        return response()->json($result,200)->header('Content-type', 'application/json');
+    }
+    
+    private function findObject($class,$search)
+    {
+        $result = $this->getResults($class,$search);
+        return $this->prepareResults($class,$result);
+    }
+    
+    private function getResults($class,$search)
+    {
+        $namespace = Classes::getNamespaceOfClass($class);
+        switch ($class) {
+            case 'Person':
+            case 'Friend':
+            case 'FamilyMember':
+                return $namespace::search()->where('lastname','begins with',$search)->orWhere('firstname','begins with',$search)->get();
+        }
+    }
+    
+    private function prepareResults($class,$results)
+    {
+        $return = [];
+        foreach ($results as $result) {
+            $obj = new \StdClass();
+            $obj->id = $result->id;
+            switch ($class) {
+                case 'Person':
+                case 'Friend':
+                case 'FamilyMember':
+                    $obj->name = $result->lastname.", ".$result->firstname;
+                    break;
+            }
+        }
+    }
+}
