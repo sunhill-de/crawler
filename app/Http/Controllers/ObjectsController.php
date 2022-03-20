878ca88649c176a9e07eaee6616e9925fd3020c6
Klaus Dimde <klaus@dimde.info>
2022-03-20 04:24:01 +0100

    Implemented add objects

--- app/Http/Controllers/ObjectsController.php
+++ app/Http/Controllers/ObjectsController.php
@@ -4,17 +4,28 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Http\Request;
+use Psr\Http\Message\ServerRequestInterface;
 use Sunhill\ORM\Facades\Classes;
 use Sunhill\ORM\Facades\Objects;
 
 class ObjectsController extends Controller
 {
+ 
+    private function getFixedInheritance(string $class)
+    {
+        if ($class == 'object') {
+            return ['object'];
+        } else {
+            return Classes::getInheritanceOfClass($class,true);
+        }        
+    }
     
     private function getBestTemplate(string $class, string $action)
     {
-        $base = 'objects.$action.';
+        $base = "objects.$action.";
         $path = resource_path("views/objects/$action/");
-        $inheritance = Classes::getInheritanceOfClass($class,true);
+        $inheritance = $this->getFixedInheritance($class);
         foreach ($inheritance as $class_entry) {
             $class_entry = strtolower($class_entry);
             if (file_exists($path.$class_entry.'.blade.php')) {
@@ -33,13 +44,15 @@ class ObjectsController extends Controller
     public function list($class='object')
     {
         $objects = Objects::getObjectList($class);
+        $pass_objects = [];
         foreach ($objects as $object) {
-            if ($object->id == 0) {
-                
+            if ($object && ($object->id > 0)) {
+                $pass_objects[] = $object;
             }
         }
         return view($this->getBestTemplate($class,'list'), [
-            'objects'=>$objects
+            'inheritance'=>array_reverse($this->getFixedInheritance($class)),
+            'objects'=>$pass_objects
         ]);        
     }
     
@@ -57,12 +70,57 @@ class ObjectsController extends Controller
     
     public function add($class)
     {
-        return view($this->getBestTemplate($class,'add'), [
-        ]);        
+        $template = $this->getBestTemplate($class,'add');
+        
+        if ($template == 'objects.add.object') {
+            return view($template, [
+               'class'=>$class,
+               'fields'=>$this->getFields($class) 
+            ]);
+        } else {
+            return view($template, [
+                'class'=>$class,            
+            ]);
+        }
     }
     
-    public function exec_add()
+    private function getFields(string $class)
     {
+        
+        $namespace = Classes::getNamespaceOfClass($class); 
+        $namespace::initializeProperties();// @todo this is a hack for a ORM-Bug!
+        $properties = $namespace::staticGetPropertiesWithFeature();
+        
+        $result = [];
+        foreach ($properties as $property) {
+            if ($property->get_editable()) {
+                $element = new \StdClass();
+                $element->name = $property->getName();
+                $element->type = $property->getType();
+                $element->default = $property->getDefault();
+                if ($property->getType() == 'Enum') {
+                    $element->enumvalues = $property->getEnumValues();
+                } else {
+                    $element->enumvalues = [];
+                }
+                $result[] = $element;
+            }
+        }
+        
+        return $result;
+    }
+    
+    public function exec_add(Request $request, $class)
+    {
+        $object = Classes::createObject($class);
+        $input = $request->all();
+        foreach ($input as $key => $value) {
+            if ($key[0] !== '_') {
+                $object->$key = $value;
+            }
+        }
+        $object->commit();
+        return $this->list($class);
     }
     
     public function edit($objectid)
