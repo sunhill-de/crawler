354a0965569ea05f2fd29ef41ab694c9454a72c9
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-03-22 13:28:27 +0100

    Create City.php

---
+++ lib/Objects/City.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * @file City.php
+ * Provides informations about a city
+ * Lang en
+ * Reviewstatus: 2022-03-22
+ * Localization: unknown
+ * Documentation: unknown
+ * Tests: unknown
+ * Coverage: unknown
+ * Dependencies: Location
+ */
+namespace Sunhill\Crawler\Objects;
+
+/**
+ * The class for cities
+ *
+ * @author lokal
+ *        
+ */
+class Property extends ORMObject
+{
+    public static $table_name = 'properties';
+    
+    public static $object_infos = [
+        'name'=>'City',       // A repetition of static:$object_name @todo see above
+        'table'=>'cities',     // A repitition of static:$table_name
+        'name_s' => 'city',
+        'name_p' => 'cities',
+        'description' => 'Class for cities',
+        'options'=>0,           // Reserved for later purposes
+    ];
+    
+    protected static function setupProperties()
+    {
+        parent::setupProperties();
+        self::varchar('zip')
+            ->setMaxLen(10)
+            ->set_description('The zip of this city')
+            ->set_displayable(true)
+            ->set_editable(true)
+            ->set_groupeditable(false)
+            ->searchable();
+        self::varchar('area_code')
+            ->setMaxLen(10)
+            ->set_editable(true)
+            ->set_groupeditable(true)
+            ->set_displayable(true);
+    }
+}
