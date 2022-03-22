1c12ec880acf42d059af3e96ee386d1d927d6815
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-03-22 12:07:20 +0100

    Create PersonsRelation.php

---
+++ lib/Objects/PersonsRelation.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * @file PersonsRelation.php
+ * Provides informations about a relation between two persons
+ * Lang en
+ * Reviewstatus: 2022-03-17
+ * Localization: unknown
+ * Documentation: unknown
+ * Tests: unknown
+ * Coverage: unknown
+ * Dependencies: ORMObject
+ */
+namespace Sunhill\Crawler\Objects;
+
+use Sunhill\ORM\Objects\ORMObject;
+
+/**
+ * The class for properties
+ *
+ * @author lokal
+ *        
+ */
+class PersonRelations extends ORMObject
+{
+    public static $table_name = 'personrelations';
+    
+    public static $object_infos = [
+        'name'=>'PersonsRelation',       // A repetition of static:$object_name @todo see above
+        'table'=>'personrelations',     // A repitition of static:$table_name
+        'name_s' => 'persons relation',
+        'name_p' => 'persons relations',
+        'description' => 'Class for relation between two persons',
+        'options'=>0,           // Reserved for later purposes
+    ];
+    
+    protected static function setupProperties()
+    {
+        parent::setupProperties();
+        self::object('person1')
+            ->setAllowedObjects('Person')
+            ->searchable()
+            ->set_editable(true)
+            ->set_groupeditable(true)
+            ->set_displayable(true);
+        self::object('person2')
+            ->setAllowedObjects('Person')
+            ->searchable()
+            ->set_editable(true)
+            ->set_groupeditable(true)
+            ->set_displayable(true)
+            ->setAllowedObjects(['Location']);
+        self::enum('relation')
+            ->searchable()
+            ->set_editable(true)
+            ->set_groupeditable(true)
+            ->set_displayable(true)
+            ->setEnumValues(['marriage','relation','divorce']);
+        self::date('relation_date')
+            ->searchable()
+            ->set_editable(true)
+            ->set_groupeditable(true)
+            ->set_displayable(true);
+    }
+}
